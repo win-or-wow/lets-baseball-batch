9faf3309a03cdcc40a6c5e4db9f2c4ee25a32957
j-jeongeun <jejeong88@gmail.com>
2024-08-15 19:53:16 +0900

    first commit

---
+++ src/main/java/com/korea/baseball_batch/BaseballBatchApplication.java
@@ -0,0 +1,13 @@
+package com.korea.baseball_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korea/baseball_batch/BaseballBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korea.baseball_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
